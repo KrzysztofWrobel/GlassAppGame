7bdb085f510082d3ab81eb03fe956e45785a07b0
Michal Kisiel <mkisiel89@gmail.com>
2013-07-06 18:07:04 +0200

    navigate to fragment

---
+++ src/com/github/krzysztofwrobel/glassappgame/fragments/NavigateFragment.java
@@ -0,0 +1,26 @@
+package com.github.krzysztofwrobel.glassappgame.fragments;
+
+import com.github.krzysztofwrobel.glassappgame.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class NavigateFragment extends Fragment
+{
+
+    public static NavigateFragment getInstance()
+    {
+    	NavigateFragment fragment = new NavigateFragment();
+    	return fragment;
+    }
+    
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    	View v = inflater.inflate(R.layout.simple_text_layout, container, false);
+    	((TextView)v.findViewById(R.id.text)).setText(R.string.navigate_to);
+    	return v;
+    }
+}
