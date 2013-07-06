f84cb874588e180c0a8387e403f0c3debe7f5a19
Aleksander Piotrowski <alek@nic.com.pl>
2013-07-06 17:01:12 +0200

    reward i response

---
+++ src/com/hackato/GlassAppGame/models/RewardResponse.java
@@ -0,0 +1,18 @@
+package com.hackato.GlassAppGame.models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alek on 7/6/13.
+ */
+public class RewardResponse extends StdResponse {
+	private Reward reward;
+
+	public Reward getReward() {
+		return reward;
+	}
+
+	public void setReward(Reward reward) {
+		this.reward = reward;
+	}
+}
